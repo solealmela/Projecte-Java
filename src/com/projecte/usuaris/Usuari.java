37d45c85e59be77f0fbf39772d404119845127e7
RubenSant <rubensantacatalinapellicer@gmail.com>
2025-05-21 16:36:50 +0200

    aqui ya no me da error na

--- src/com/projecte/usuaris/Usuari.java
+++ src/com/projecte/usuaris/Usuari.java
@@ -1,6 +1,12 @@
 package com.projecte.usuaris;
 
-import com.projecte.entitat.Persona;
+import com.projecte.entitat.*;
 public class Usuari extends Persona {
+
+    public Usuari(String nombre, String apellido, String fechaNacimiento) {
+        super(nombre, apellido, fechaNacimiento);
+        //TODO Auto-generated constructor stub
+    }
+
 
 }
