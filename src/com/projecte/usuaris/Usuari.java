98b1f6a87071675ebb5cf9aeed6fa6d732becd55
RubenSant <rubensantacatalinapellicer@gmail.com>
2025-05-21 16:47:56 +0200

    fer usuari

--- src/com/projecte/usuaris/Usuari.java
+++ src/com/projecte/usuaris/Usuari.java
@@ -2,11 +2,21 @@ package com.projecte.usuaris;
 
 import com.projecte.entitat.*;
 public class Usuari extends Persona {
-
-    public Usuari(String nombre, String apellido, String fechaNacimiento) {
+    public Usuari(String nombre, String apellido, String fechaNacimiento, int id, boolean rol, String poblacion,
+            String nombreUsuario, String contrasena) {
         super(nombre, apellido, fechaNacimiento);
-        //TODO Auto-generated constructor stub
+        this.id = id;
+        this.rol = rol;
+        this.poblacion = poblacion;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasena = contrasena;
     }
+
+    int id;
+    boolean rol;
+    String poblacion;
+    String nombreUsuario; //Lo introducimos cada vez que se accede
+    String contrasena;
 
 
 }
