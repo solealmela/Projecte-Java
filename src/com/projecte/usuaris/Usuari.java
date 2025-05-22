eb68c33660527a458cbd0257ecf9151a20b8c3f0
RubenSant <rubensantacatalinapellicer@gmail.com>
2025-05-22 18:18:23 +0200

    Actualisar usuari

--- src/com/projecte/usuaris/Usuari.java
+++ src/com/projecte/usuaris/Usuari.java
@@ -2,21 +2,49 @@ package com.projecte.usuaris;
 
 import com.projecte.entitat.*;
 public class Usuari extends Persona {
-    public Usuari(String nombre, String apellido, String fechaNacimiento, int id, boolean rol, String poblacion,
+    String rol;
+    int id;
+    String poblacion;
+    String nombreUsuario; //Lo introducimos cada vez que se accede
+    String contrasena;
+
+    public Usuari(String nombre, String apellido, String fechaNacimiento, int id, String poblacion,
             String nombreUsuario, String contrasena) {
         super(nombre, apellido, fechaNacimiento);
         this.id = id;
-        this.rol = rol;
+        rol = "ROL.USUARI";
         this.poblacion = poblacion;
         this.nombreUsuario = nombreUsuario;
         this.contrasena = contrasena;
     }
 
-    int id;
-    boolean rol;
-    String poblacion;
-    String nombreUsuario; //Lo introducimos cada vez que se accede
-    String contrasena;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String isRol() {
+        return rol;
+    }
+    public String getPoblacion() {
+        return poblacion;
+    }
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+    public String getContrasena() {
+        return contrasena;
+    }
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
 
 
 }
