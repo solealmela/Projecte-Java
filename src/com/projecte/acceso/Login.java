3dfa32f82f520f08bf3a04102bc335fdf52ddb2c
RubenSant <rubensantacatalinapellicer@gmail.com>
2025-05-22 19:17:41 +0200

    Fer login

--- src/com/projecte/acceso/Login.java
+++ src/com/projecte/acceso/Login.java
@@ -1,5 +1,6 @@
 package com.projecte.acceso;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
@@ -9,18 +10,24 @@ public class Login {
         Scanner sc = new Scanner(System.in);
         
 try {
-    FileReader filereader = new FileReader(arxiu);
-    System.out.println("Dime el usuario");
+    System.out.println("Dime el nombre de usuario");
     String usuario=sc.nextLine();
     System.out.println("Dime la contraseña");
     String contraseña=sc.nextLine();
-
-    filereader.close();
-} catch (IOException e) {
-    System.out.println("Error al leer el archivo: " + e.getMessage());
-} finally {
-    sc.close();
-}
+    BufferedReader lector= new BufferedReader(new FileReader(arxiu));
+    String linea;
+            while ((linea = lector.readLine()) != null) {
+                System.out.println(linea);
+                String[] partes = linea.split(":");
+                if(partes[0]==usuario && partes[3]==contraseña){
+                    System.out.println("Bienvenido"+usuario);
+                }
+            }
+            lector.close();
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo: " + e.getMessage());
+        }
+    
         
         
 
