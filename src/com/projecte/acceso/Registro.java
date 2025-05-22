d6c3c1fee7c9e56cb46b5bccb0c9b645f3439fb3
Sole Pareja <solealmela1306@gmail.com>
2025-05-22 19:20:05 +0200

    principio registro

---
+++ src/com/projecte/acceso/Registro.java
@@ -0,0 +1,49 @@
+package com.projecte.acceso;
+
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class Registro {
+    
+    private Scanner sc = new Scanner(System.in);
+
+
+    public void obtenerDatos() {
+        System.out.println("Introduce tu nombre de usuario");
+        String nombreUsuario = sc.nextLine();
+        
+        System.out.println("Introduce tus apellidos");
+        String apellidos = sc.nextLine();
+        
+        System.out.println("Introduce tu email");
+        String email = sc.nextLine();
+
+        System.out.println("Introduce tu contraseña");
+        String contrasenya = sc.nextLine();
+        System.out.println("Repite la contraseña");
+        String verificarContrasenya = sc.nextLine();
+
+        System.out.println("Introduce tu población");
+        String poblacion = sc.nextLine();
+        
+        System.out.println("Introduce tu fecha de nacimiento");
+        String fechaNacimiento = sc.nextLine();
+
+
+
+    }
+
+    public boolean validarEmail(String email) {
+        return email.indexOf('@') != -1;
+    }
+
+    public boolean noExisteEmail(String email) {
+        FileReader in = null;
+
+        try {
+            in = new FileReader("archivoUsuarios.txt");
+
+            
+        }
+    }
+}
