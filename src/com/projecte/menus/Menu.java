4b6d7c37dd3a90d734dc89faa411bbb2b6344ad9
Sole Pareja <solealmela1306@gmail.com>
2025-05-23 18:52:25 +0200

    menu bien?

--- src/com/projecte/menus/Menu.java
+++ src/com/projecte/menus/Menu.java
@@ -1,27 +1,12 @@
 package com.projecte.menus;
 
-<<<<<<< HEAD
-import java.util.Scanner;
-
-public class Menu {
-    private String titulo;
-    private String [] opciones;
-    private Scanner scanner;
-
-
-    public Menu(String titulo, String[] opciones, Scanner scanner) {
-        this.titulo = titulo;
-        this.opciones = opciones;
-        this.scanner = new Scanner(System.in); // obtener el dato del escanner
-=======
 public class Menu {
    private String titulo;
    private String [] opciones;
 
     public Menu(String titulo, String[] opciones) {
-    this.titulo = titulo;
-    this.opciones = opciones;
->>>>>>> 147ea75371f79ec7a7f6722483380fded663a548
+        this.titulo = titulo;
+        this.opciones = opciones;
     }
 
     public String getTitulo() {
@@ -29,40 +14,15 @@ public class Menu {
     }
     public void setTitulo(String titulo) {
         this.titulo = titulo;
-<<<<<<< HEAD
-    }//titulo
-
-=======
     }
->>>>>>> 147ea75371f79ec7a7f6722483380fded663a548
 
     public String[] getOpciones() {
         return opciones;
     }
+
     public void setOpciones(String[] opciones) {
         this.opciones = opciones;
-<<<<<<< HEAD
-    }//opciones
 
-
-    public Scanner getScanner() {
-        return scanner;
-    }
-    public void setScanner(Scanner scanner) {
-        this.scanner = scanner;
-    }//scanner
-
-    public int mostrar(){
-        int opcion = -1;
-        // do {
-            
-        // } while (condition);
-        return opcion;
-    }
-
-    
-
-=======
     }
 
     public void mostrarMenu(){
@@ -72,7 +32,5 @@ public class Menu {
     }
     System.out.print("Selecciona una opción: ");
     }
->>>>>>> 147ea75371f79ec7a7f6722483380fded663a548
-
     
 }
