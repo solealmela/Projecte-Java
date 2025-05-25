f71b574a30c05ab1e91135b46817d113dd0f342a
RubenSant <rubensantacatalinapellicer@gmail.com>
2025-05-26 00:01:44 +0200

    crear clase pelicula

--- src/com/projecte/entidad/Pelicula.java
+++ src/com/projecte/entidad/Pelicula.java
@@ -1,5 +1,14 @@
 package com.projecte.entidad;
+
+import java.util.ArrayList;
+
 public class Pelicula {
+
+    private int id;
+    private int duracionMinutos;
+    private String nombre;
+    private ArrayList actoresDeLaPeli= new ArrayList<>();
+    private Director director;
 
     
 }
